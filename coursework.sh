d627faa89557f69847eb428cb7fc94ee13b742bf
bombomdragon <3430955728@qq.com>
2025-10-25 16:36:46 +0000

    Complete the linux coursework

--- coursework.sh
+++ coursework.sh
@@ -33,15 +33,16 @@
 # 提示：先进入 generated 目录，然后使用创建子目录，创建完成后回到上级目录
 
 # 请在下方写下你的命令（删除下面的 echo 语句并替换为你的命令，下面每一题都同理）
-echo "任务 1: 请完成创建目录结构的命令"
-
+cd ./generated/ && mkdir -p workspace/{scripts,data}
+cd ../
 
 # 任务 2: 创建文件
 # 要求：在 ./generated/workspace/scripts/ 目录下创建两个空文件：
 #       hello.sh 和 utils.sh
 
 # 请在下方写下你的命令
-echo "任务 2: 请完成创建文件的命令"
+cd ./generated/workspace/scripts/ && touch hello.sh utils.sh
+cd ../../../
 
 
 # 任务 3: 创建文件内容
@@ -51,21 +52,22 @@ echo "任务 2: 请完成创建文件的命令"
 # 提示：可以自行上网搜索如何一次性写入包含多行的文件
 
 # 请在下方写下你的命令
-echo "任务 3: 请完成创建文件内容的命令"
-
+cd ./generated/workspace/scripts/ && echo -e "#!/bin/bash\necho \"Hello, Linux!\"" > hello.sh
+cd ../../../
 
 # 任务 4: 删除文件
 # 要求：删除 ./generated/workspace/scripts/utils.sh 文件
 
 # 请在下方写下你的命令
-echo "任务 4: 请完成删除文件的命令"
+rm ./generated/workspace/scripts/utils.sh
 
 
 # 任务 5: 使用 rm 删除目录
 # 要求：删除 ./generated/workspace/data/ 目录
 
 # 请在下方写下你的命令
-echo "任务 5: 请完成删除目录的命令"
+cd ./generated/workspace/ && rm -r data/
+cd ../../
 
 
 # 任务 6: 使用 cat 和 grep 创建和搜索文件
@@ -78,7 +80,8 @@ echo "任务 5: 请完成删除目录的命令"
 # 提示：可以自行上网搜索如何一次性写入包含多行的文件
 
 # 请在下方写下你的命令
-echo "任务 6: 请完成创建文件和搜索的命令"
+cd ./generated/workspace/ && touch info.txt && echo -e "Linux is a powerful operating system.\nBash scripting is fun and useful.\nCommand line tools are essential for developers." > info.txt && grep "Linux" info.txt > result.txt
+cd ../../
 
 
 # ============================================================================
